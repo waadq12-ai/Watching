03f4d176d3c696952721ede59f796a55badaeb1c
waadq12-ai <waad.q.12@gmail.com>
2026-07-01 09:13:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`البوت جاهز: ${client.user.tag}`);
+});
+
+// هنا سنضع التوكن لاحقاً في إعدادات Render
+client.login(process.env.TOKEN);
 
